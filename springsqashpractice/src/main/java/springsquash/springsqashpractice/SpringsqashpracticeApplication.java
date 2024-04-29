02c1f359a4e329a0cd46e53cbbd5a1a955b3e008
vikramgundkar1 <110653893+vikramgundkar1@users.noreply.github.com>
2024-04-29 10:28:35 +0530

    First master commit

---
+++ springsqashpractice/src/main/java/springsquash/springsqashpractice/SpringsqashpracticeApplication.java
@@ -0,0 +1,16 @@
+package springsquash.springsqashpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsqashpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsqashpracticeApplication.class, args);
+
+
+		System.out.println("This is update for main branch");
+	}
+
+}
---
+++ springsqashpractice/src/test/java/springsquash/springsqashpractice/SpringsqashpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package springsquash.springsqashpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsqashpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
